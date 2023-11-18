0bfe91b9e14a373c0fac9db1ff4fb41ddd7670b7
Karel Tutsu <KarelOmab@gmail.com>
2023-11-18 11:59:55 -0500

    remove redundant abstract method

--- classes/account/user.py
+++ classes/account/user.py
@@ -4,8 +4,3 @@ class User(Account):
     def __init__(self, id: int, username: str):
         super().__init__(id, username)
         self._privileged_mode = False
-
-    @property
-    def privileged_mode(self) -> bool:
-        """Indicates that a regular user does not have privileged mode."""
-        return self._privileged_mode
